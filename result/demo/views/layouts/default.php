2184c742d80e39367c36e6d2e5d70e4021f8a943
buddh4 <mail@jharrer.de>
2018-01-16 13:33:58 +0100

    Removed test modules

--- result/demo/views/layouts/default.php
+++
@@ -1,9 +0,0 @@
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="panel panel-default">
-                    <?= $content ?>
-            </div>
-        </div>
-    </div>
-</div>
--- result/example/views/admin/index.php
+++
@@ -1,9 +0,0 @@
-<div class="container-fluid">
-    <div class="panel panel-default">
-        <div class="panel-heading"><strong>Example</strong> <?= Yii::t("ExampleModule.base", "configuration") ?></div>
-
-        <div class="panel-body">
-            <p><?= Yii::t("ExampleModule.base", "Welcome to the admin only area.") ?></p>
-        </div>
-    </div>
-</div>
