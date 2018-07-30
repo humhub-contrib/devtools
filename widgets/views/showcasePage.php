e616de2f3396bfc99ec08834f0bfdb84feb2dd07
Daniel Kesselberg <mail@danielkesselberg.de>
2018-07-30 12:57:45 +0200

    :bug: Fix broken modal showcases on humhub 1.3

--- widgets/views/showcasePage.php
+++ widgets/views/showcasePage.php
@@ -1,23 +1,24 @@
 <?php
 
+use humhub\modules\devtools\assets\DevtoolsAsset;
+use humhub\modules\devtools\widgets\Showcase;
 use humhub\widgets\Button;
 use yii\helpers\Url;
 
-\humhub\modules\devtools\assets\DevtoolsAsset::register($this);
+DevtoolsAsset::register($this);
 ?>
 <div class="showcase-page panel panel-default">
     <div class="panel-heading">
         <?= Yii::t('DevtoolsModule.base', '<strong>Developer</strong> Tools'); ?>
     </div>
-   
+
     <div class="panel-body">
         <div class="clearfix">
             <?= Button::back(Url::to(['/devtools/index'])) ?>
         </div>
-        <br />
-        <?php foreach ($showcases as $showcase) : ?>
-            <?= \humhub\modules\devtools\widgets\Showcase::widget($showcase) ?>
+        <br/>
+        <?php foreach ($showcases as $showcase): ?>
+            <?= Showcase::widget($showcase) ?>
         <?php endforeach; ?>
     </div>
 </div>
-
